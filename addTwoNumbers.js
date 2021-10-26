52932eadda851a32d3271fb83f60dc0698c959f1
moise10r <moiserushanika2006@gmail.com>
2021-10-26 11:24:24 +0200

    Add twosum challenge

---
+++ addTwoNumbers.js
@@ -0,0 +1,7 @@
+var addTwoNumbers = function(l1, l2) {
+  num1 = l1.join('')
+  num2 = l2.join('')
+  return num1
+};
+
+console.log(addTwoNumbers([2,3,6],[5,7,2]));
--- twoSum.js
+++ twoSum.js
@@ -1,10 +1,4 @@
-// var twoSum = function(nums, target) {
-//   for(let i = 0; i < nums.length; i++) {
-//       if(nums[i]+nums[i+1] === target) return [i, i+1]
-//   }
-// };
 
-// console.log(twoSum([3,2,3] ,6))
 const nums = [3,2,3];
 const target = 6;
 
